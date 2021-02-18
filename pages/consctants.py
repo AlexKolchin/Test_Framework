c6dcfddcdd0260c01a689d40dc739885511084b1
okolchin-amplify <okolchin@amplify.com>
2021-02-18 13:57:28 +0200

    lists removed to constants

--- pages/consctants.py
+++ pages/consctants.py
@@ -1,3 +1,6 @@
 MAIN_PAGE = "https://refactoring.guru/"
 LOGIN_PAGE = "https://refactoring.guru/login/"
 MAIN_PAGE_CN = "https://refactoringguru.cn/"
+LANGUAGES = ['', 'es', 'fr', 'pl', 'pt-br', 'cn', 'ru', 'uk']
+HELLO_WORLD = ['Hello, world!', '¡Hola, mundo!', 'Hello, world!', 'Hello, world!',
+                  'Olá mundo!', '你好， 世界！', 'Hello, world! Привет!', 'Hello, world! Привіт!']
--- test/test_main_page.py
+++ test/test_main_page.py
@@ -1,6 +1,5 @@
 from pages.main_page import MainPage
 from pages.consctants import *
-import time
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -17,18 +16,15 @@ def test_guest_should_see_login_link(browser):
 
 def test_languages(browser):
     i = 0
-    url_list = ['', 'es', 'fr', 'pl', 'pt-br', 'cn', 'ru', 'uk']
-    hello_list = ['Hello, world!', '¡Hola, mundo!', 'Hello, world!', 'Hello, world!',
-                  'Olá mundo!', '你好， 世界！', 'Hello, world! Привет!', 'Hello, world! Привіт!']
-    for lang in url_list:
+    for lang in LANGUAGES:
         if lang == 'cn':
             page = MainPage(browser, MAIN_PAGE_CN)
             page.open()
-            assert hello_list[i] == page.header_one_selector_cn().text, "Error"
+            assert HELLO_WORLD[i] == page.header_one_selector_cn().text, "Error"
             i += 1
         else:
             page = MainPage(browser, MAIN_PAGE+lang)
             page.open()
-            assert hello_list[i] == page.header_one_selector().text, "Error"
+            assert HELLO_WORLD[i] == page.header_one_selector().text, "Error"
             i += 1
 
